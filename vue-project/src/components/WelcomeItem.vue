a7ed7dbd1b218814f2c6885e25dc1243ecddfefc
andreaguo <113132239+andreaguo@users.noreply.github.com>
2023-03-20 23:36:55 -0400

    gh

--- vue-project/src/components/WelcomeItem.vue
+++ vue-project/src/components/WelcomeItem.vue
@@ -17,12 +17,10 @@
   margin-top: 2rem;
   display: flex;
 }
-
 .details {
   flex: 1;
   margin-left: 1rem;
 }
-
 i {
   display: flex;
   place-items: center;
@@ -31,20 +29,17 @@ i {
   height: 32px;
   color: var(--color-text);
 }
-
 h3 {
   font-size: 1.2rem;
   font-weight: 500;
   margin-bottom: 0.4rem;
   color: var(--color-heading);
 }
-
 @media (min-width: 1024px) {
   .item {
     margin-top: 0;
     padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
   }
-
   i {
     top: calc(50% - 25px);
     left: -26px;
@@ -55,7 +50,6 @@ h3 {
     width: 50px;
     height: 50px;
   }
-
   .item:before {
     content: ' ';
     border-left: 1px solid var(--color-border);
@@ -64,7 +58,6 @@ h3 {
     bottom: calc(50% + 25px);
     height: calc(50% - 25px);
   }
-
   .item:after {
     content: ' ';
     border-left: 1px solid var(--color-border);
@@ -73,11 +66,9 @@ h3 {
     top: calc(50% + 25px);
     height: calc(50% - 25px);
   }
-
   .item:first-of-type:before {
     display: none;
   }
-
   .item:last-of-type:after {
     display: none;
   }
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 v-if="selected === 'A'">Correct Answer</h1>
+  <h1 v-else-if="selected === ''">Select an answer</h1>
+  <h1 v-else>"Wrong"</h1>
+  <h1>{{ student }}</h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+  </ul>
+  <select v-model="selected">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <span>{{ selected }}</span>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      student: "Jason",
+      animals: ["pig", "cow", "kwan", "Zee"],
+      graduated: true,
+      selected: "",
+    };
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+h1{
+color: red;
+}
+
+</style>
